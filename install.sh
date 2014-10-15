3bf67eb058078d289c1977b0325c807ddadc0841
Jörg Kütemeier <jkuetemeier@jkuetemeier.com>
2014-10-15 21:54:20 +0200

    work on install script

--- install.sh
+++ install.sh
@@ -1,4 +1,73 @@
 #!/usr/bin/env sh
 
-./zsh/install.sh
-./vim/install.sh
+usage()
+{
+cat << EOF
+usage: $0 options
+
+This script can be installed local ($HOME) or global.
+
+OPTIONS:
+   -h      Show this message
+   -l      install local ($HOME/.jk-sys)
+   -g      install global (/opt/jk-sys)
+   -v      Verbose
+
+EOF
+}
+
+GLOBAL=
+LOCAL=
+VERBOSE=
+
+while getopts “ghlv” OPTION
+do
+case $OPTION in
+ g)
+     GLOBAL=1
+     ;;
+ h)
+     usage
+     exit 1
+     ;;
+ l)
+     LOCAL=1
+     ;;
+ v)
+     VERBOSE=1
+     ;;
+ ?)
+     usage
+     exit
+     ;;
+esac
+done
+
+if [[ -z $GLOBAL ]] && [[ -z $LOCAL ]]; then
+  usage
+  exit 1
+fi
+
+if [[ -n $GLOBAL ]] && [[ -n $LOCAL ]]; then
+  echo "ERROR: You can install only global OR local"
+  usage
+  exit 1
+fi
+
+OPT=-l
+if [[ -n $GLOBAL ]]; then
+  OPT=-g
+fi
+
+if [[ -n $VERBOSE ]]; then
+  if [[ -n $GLOBAL ]]; then
+    echo "starting global install..."
+  else 
+    echo "starting local install..."
+  fi
+  OPT="$OPT -v"
+fi
+
+./zsh/install.sh $OPT
+./vim/install.sh $OPT
+
